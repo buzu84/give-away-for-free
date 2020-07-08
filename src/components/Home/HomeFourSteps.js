bb3905880eb076c70ffebfa2990c77a91b0340d4
Magdalena Buzikiewicz <buzu@op.pl>
2020-07-08 23:33:19 +0200

    Feature1 in progress.

--- src/components/Home/HomeFourSteps.js
+++ src/components/Home/HomeFourSteps.js
@@ -1,8 +1,51 @@
-import React from "react";
+// import React from "react";
+//
+// const HomeFourSteps = () => {
+//   return (
+//       <h1 className="steps" style={{height: 400}}>4 steps</h1>
+//   );
+// }
+//
+// export default HomeFourSteps;
 
-const HomeFourSteps = () => {
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const HomeFourSteps = (props) => {
   return (
-      <h1 className="steps" style={{height: 400}}>4 steps</h1>
+    <section className="steps info-section container" style={{height: 400}}>
+      <div className="info-item">
+        <div className="info-box">
+          <div className="icon"><i className="fas fa-check"></i></div>
+          <h2>Golym okiem</h2>
+          <p>
+            Znajdź miejsca o odpowiednich parametrach do oglądania nocnego nieba, możesz też dodać nowy punkt obserwacyjny do naszej bazy miejscówek...
+          </p>
+        </div>
+        <div className="info-box">
+          <div className="icon"><i className="far fa-clock"></i></div>
+          <h2>Meteoryty</h2>
+          <p>
+            Badz na biezaco z wydarzeniami na niebie, zaplanuj kiedy wybrać się na nocny spektakl deszczu meteorytow.
+          </p>
+        </div>
+        <div className="info-box">
+          <div className="icon"><i className="fas fa-align-left"></i></div>
+          <h2>A gdy pogoda nie dopisze..</h2>
+          <p>
+            Skorzystaj z bazy obserwatoriow astronomicznych w Twojej okolicy i wybierz się na ekscytujący seans z kosmosem...
+          </p>
+        </div>
+        <div className="info-box">
+          <div className="icon"><i className="fas fa-align-left"></i></div>
+          <h2>A gdy pogoda nie dopisze..</h2>
+          <p>
+            Skorzystaj z bazy obserwatoriow astronomicznych w Twojej okolicy i wybierz się na ekscytujący seans z kosmosem...
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
 
