f80be0570ceaab1bf92cc6607a3441aff475e456
Magdalena Buzikiewicz <buzu@op.pl>
2020-07-09 08:17:30 +0200

    Feature1 in progress.

--- src/components/Home/HomeFourSteps.js
+++ src/components/Home/HomeFourSteps.js
@@ -9,39 +9,47 @@
 // export default HomeFourSteps;
 
 import React from "react";
-import ReactDOM from "react-dom";
+
+import myPic_1 from '../../assets/Icon-1.svg';
+import myPic_2 from '../../assets/Icon-2.svg';
+import myPic_3 from '../../assets/Icon-3.svg';
+import myPic_4 from '../../assets/Icon-4.svg';
+import mySvg from '../../assets/Decoration.svg';
 
 
 const HomeFourSteps = (props) => {
   return (
     <section className="steps info-section container" style={{height: 400}}>
-      <div className="info-item">
-        <div className="info-box">
-          <div className="icon"><i className="fas fa-check"></i></div>
-          <h2>Golym okiem</h2>
+      <h1>Wystarczą cztery proste kroki</h1>
+      <img src={mySvg} alt="decoration" />
+      <div className="info-item info-item-2">
+        <div className="info-box info-box-2">
+          <div className="icon"><img src={myPic_1} alt="tshirt" /></div>
+          <h2>Wybierz rzeczy</h2>
           <p>
-            Znajdź miejsca o odpowiednich parametrach do oglądania nocnego nieba, możesz też dodać nowy punkt obserwacyjny do naszej bazy miejscówek...
+            ubrania, zabawki, sprzęt i inne
           </p>
         </div>
-        <div className="info-box">
-          <div className="icon"><i className="far fa-clock"></i></div>
-          <h2>Meteoryty</h2>
+        <div className="info-box info-box-2">
+          <div className="icon"><img src={myPic_2} alt="box" /></div>
+          <h2>Spakuj je</h2>
           <p>
-            Badz na biezaco z wydarzeniami na niebie, zaplanuj kiedy wybrać się na nocny spektakl deszczu meteorytow.
+            Skorzystaj z worków na śmieci
           </p>
         </div>
-        <div className="info-box">
-          <div className="icon"><i className="fas fa-align-left"></i></div>
-          <h2>A gdy pogoda nie dopisze..</h2>
+        <div className="info-box info-box-2">
+          <div className="icon"><img src={myPic_3} alt="refresh" /></div>
+          <h2>Zdecyduj komu chcesz pomóc</h2>
           <p>
-            Skorzystaj z bazy obserwatoriow astronomicznych w Twojej okolicy i wybierz się na ekscytujący seans z kosmosem...
+            wybierz zaufane miejsce
           </p>
+
         </div>
-        <div className="info-box">
-          <div className="icon"><i className="fas fa-align-left"></i></div>
-          <h2>A gdy pogoda nie dopisze..</h2>
+        <div className="info-box info-box-2">
+          <div className="icon"><img src={myPic_4} alt="search" /></div>
+          <h2>Zamów kuriera</h2>
           <p>
-            Skorzystaj z bazy obserwatoriow astronomicznych w Twojej okolicy i wybierz się na ekscytujący seans z kosmosem...
+            kurier przyjedzie w dogodnym terminie
           </p>
         </div>
       </div>
